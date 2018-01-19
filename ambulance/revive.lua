b9d72522f881fb9522845773e3c11c3c40172f7f
TanguyOrtegat <tanguy.ortegat@student.odisee.be>
2018-01-19 09:36:21 +0100

    code fix + optimalisation

--- ambulance/revive.lua
+++ ambulance/revive.lua
@@ -15,24 +15,24 @@ ____________________________________________________________________________
 
 
 TriggerEvent('es:addGroupCommand', 'revivea', 'user', function(source, args, user)
-    local source = source
-    local admin = 0
-    local xPlayers = ESX.GetPlayers()
-    for i=1, #xPlayers, 1 do
-        local xPlayer = ESX.GetPlayerFromId(xPlayers[i])
-        if xPlayer.group == 'admin' or xPlayer.group == 'superadmin'  then
-            admin = admin + 1
-        end
-    end
+	local xPlayers = ESX.GetPlayers()
+	local AdminsConnected = 0
 
-if(admin >= 1)then
-  if args[2] ~= nil then
-    TriggerClientEvent('esx_ambulancejob:revive', tonumber(args[2]))
-  else
-    TriggerClientEvent('esx_ambulancejob:revive', source)
-  end
-  end
-
-end, function(source, args, user)
-  TriggerClientEvent('chatMessage', source, "SYSTEM", {255, 0, 0}, "Il y a déjà des admins sur le serveur.")
+	for i=1, #xPlayers, 1 do
+		local xPlayer = ESX.GetPlayerFromId(xPlayers[i])
+		if xPlayer.getGroup() == 'admin' or xPlayer.getGroup() == 'superadmin' then
+			AdminsConnected = AdminsConnected + 1
+			
+		end
+	end
+	if AdminsConnected > 0 then
+		TriggerClientEvent('esx:showNotification', source, "Il y a déjà des admins sur le serveur.")
+	return
+	end
+	
+	if args[2] ~= nil then
+		TriggerClientEvent('esx_ambulancejob:revive', tonumber(args[2]))
+	else
+		TriggerClientEvent('esx_ambulancejob:revive', source)
+	end
 end, {help = _U('revive_help'), params = {{name = 'id'}}})
