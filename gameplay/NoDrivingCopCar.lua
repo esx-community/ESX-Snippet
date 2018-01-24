e4a57d7e631df9fad16274be896dcfc51f157760
charly <charly.strn@gmail.com>
2018-01-24 15:52:45 +0100

    replace drwTxt by notification

--- gameplay/NoDrivingCopCar.lua
+++ gameplay/NoDrivingCopCar.lua
@@ -21,7 +21,7 @@ Citizen.CreateThread(function()
             local veh = GetVehiclePedIsUsing(GetPlayerPed(PlayerId()), false)
             if (GetPedInVehicleSeat(veh, -1) == GetPlayerPed(PlayerId())) then
                 if PlayerData.job ~= nil and PlayerData.job.name ~= 'police' then
-                  drawTxt("~r~It's against the rules for civilians to drive these vehicles!",0,1,0.5,0.8,0.6,255,255,255,255)
+                  ESX.ShowNotification("Une voiture de polie n'est pas réservée aux civils..")
                   SetVehicleUndriveable(veh, true)
                 end
             end
