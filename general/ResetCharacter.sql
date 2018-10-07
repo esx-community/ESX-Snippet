846e5d7ce85f5f12ed1c4cf4a5ca0de5f1a524c5
Doxylamin <Doxylamin@users.noreply.github.com>
2018-10-07 15:11:48 +0200

    Update ResetCharacter.sql
    Only one line-change needed from now on.

--- general/ResetCharacter.sql
+++ general/ResetCharacter.sql
@@ -1,19 +1,20 @@
--- replace 'steam:110000xxxxxx' by steam of the one you want to delete
+-- replace @steamid by steam of the one you want to delete
+SET @steamid = 'steam:110000xxxxxx';
 
-DELETE FROM addon_account_data WHERE owner = 'steam:110000xxxxxx';
-DELETE FROM addon_inventory_items WHERE owner = 'steam:110000xxxxxx';
-DELETE FROM billing WHERE identifier = 'steam:110000xxxxxx';
-DELETE FROM billing WHERE sender = 'steam:110000xxxxxx';
-DELETE FROM datastore_data WHERE owner = 'steam:110000xxxxxx';
-DELETE FROM owned_properties WHERE owner = 'steam:110000xxxxxx';
-DELETE FROM owned_vehicles WHERE owner = 'steam:110000xxxxxx';
-DELETE FROM playersTattoos WHERE identifier = 'steam:110000xxxxxx';
-DELETE FROM rented_vehicles WHERE owner = 'steam:110000xxxxxx';
-DELETE FROM user_accounts WHERE identifier = 'steam:110000xxxxxx';
-DELETE FROM user_contacts WHERE identifier = 'steam:110000xxxxxx';
-DELETE FROM user_inventory WHERE identifier = 'steam:110000xxxxxx';
-DELETE FROM user_licenses WHERE owner = 'steam:110000xxxxxx';
-DELETE FROM user_parkings WHERE identifier = 'steam:110000xxxxxx';
-DELETE FROM user_contacts WHERE number IN (SELECT phone_number FROM users WHERE 'steam:110000xxxxxx');
-DELETE FROM characters WHERE identifier = 'steam:110000xxxxxx';
-DELETE FROM users WHERE identifier = 'steam:110000xxxxxx';
+DELETE FROM addon_account_data WHERE owner = @steamid;
+DELETE FROM addon_inventory_items WHERE owner = @steamid;
+DELETE FROM billing WHERE identifier = @steamid;
+DELETE FROM billing WHERE sender = @steamid;
+DELETE FROM datastore_data WHERE owner = @steamid;
+DELETE FROM owned_properties WHERE owner = @steamid;
+DELETE FROM owned_vehicles WHERE owner = @steamid;
+DELETE FROM playersTattoos WHERE identifier = @steamid;
+DELETE FROM rented_vehicles WHERE owner = @steamid;
+DELETE FROM user_accounts WHERE identifier = @steamid;
+DELETE FROM user_contacts WHERE identifier = @steamid;
+DELETE FROM user_inventory WHERE identifier = @steamid;
+DELETE FROM user_licenses WHERE owner = @steamid;
+DELETE FROM user_parkings WHERE identifier = @steamid;
+DELETE FROM user_contacts WHERE number IN (SELECT phone_number FROM users WHERE @steamid);
+DELETE FROM characters WHERE identifier = @steamid;
+DELETE FROM users WHERE identifier = @steamid;
